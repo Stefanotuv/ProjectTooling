df26f697605d8aec4a6ee306c2d2b0d68a2ed39e
stefanotuv <ste.tuveri@gmail.com>
2019-01-02 01:18:13 +0000

    Add the view for the masterdata app
    Add views and url 
    
    the new url is localhost:8000/masterdata

---
+++ DTS/masterdata/urls.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.masterdataview, name='masterdataview'),
+"""
+    path('stefano/', views.another, name='another'),
+"""
+]
