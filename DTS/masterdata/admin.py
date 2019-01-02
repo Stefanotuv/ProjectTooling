10ac31662c6913961e6fe1ff3be6ef980343af03
stefanotuv <ste.tuveri@gmail.com>
2019-01-02 21:07:51 +0000

    succefully created dockerfile to create the databases and register a superuser

--- DTS/masterdata/admin.py
+++ DTS/masterdata/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from masterdata.models import Application, AppType, Region
 
 # Register your models here.
+
+admin.site.register(Application)
+admin.site.register(AppType)
+admin.site.register(Region)
