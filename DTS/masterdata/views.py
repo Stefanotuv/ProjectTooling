1c0ab9d75d816faeb85b3074b82f3f226b453eb6
stefanotuv <ste.tuveri@gmail.com>
2019-01-02 16:50:44 +0000

    create the template for masterdata
    the template works with the data insertion

--- DTS/masterdata/views.py
+++ DTS/masterdata/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 
 def masterdataview(request):
-    return HttpResponse("MasterData view!")
+# this is the old response. The new version uses the template
+# return HttpResponse("MasterData view!")
+    masterdata_dict = {
+        'bodymasterdata':"Hello the message is now coming from view.py for masterdata"
+    }
+    return render(request,'masterdata/index.html',context=masterdata_dict)
